0187edf686aa5333357d9bab7b724e2a8d4ac492
Marc Rozendaal <rozendaalmarc@gmail.com>
2023-06-24 01:14:10 +0200

    Added pickup logic backend

---
+++ Sem_Oz/Assets/01_Scripts/Enums.cs
@@ -0,0 +1,9 @@
+
+public enum ItemType {
+    None = 0,
+    Milk = 1,
+    Plank = 2,
+    Solar_Panel = 3,
+    Book = 4,
+    Tomato_Plant = 5
+}
---
+++ Sem_Oz/Assets/01_Scripts/NPCController.cs
@@ -0,0 +1,19 @@
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPCController : MonoBehaviour {
+
+    public bool hasItem;
+    public ItemType wantedItemType;
+    
+    private void Start() {
+        
+    }
+
+    private void Update() {
+        
+    }
+    
+}
